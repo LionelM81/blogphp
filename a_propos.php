991a3d7e9caee08f552340e45fdf3a4144b52fa6
Lionel <lionelmartinez7@gmail.com>
2025-01-30 13:34:56 +0100

    bootstrap et mise page

---
+++ a_propos.php
@@ -0,0 +1,9 @@
+<?php require_once __DIR__ . "/templates/header.php"; ?>
+
+<h1>A propos</h1>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+    Est aliquam magni dolorum repellat praesentium, voluptate ratione?
+    Officia iure nisi distinctio commodi cum vero, quasi illo earum nobis nihil, dolorem odit.</p>
+
+<?php require_once __DIR__ . "/templates/footer.php"; ?>
